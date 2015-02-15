b05be389519899148facd2e5a855112f2715fb29
Steve Mitchell <SMitchell@groupon.com>
2015-02-14 20:16:02 -0600

    finish railserizing specs n stuff

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,15 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+require File.expand_path("../../config/environment", __FILE__)
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'fixtures/test_model'
 require 'fixtures/three_column'
 require 'fixtures/reserved_word_model'
 require 'rspec'
+require 'rspec/rails'
 require 'rspec/autorun'
 
 RSpec.configure do |config|
   #config.use_transactional_fixtures = false
+  config.expose_current_running_example_as :example
+  config.infer_spec_type_from_file_location!
 end
